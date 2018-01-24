845005b6b6dd5072c8ed9b9041851f71567da5c0
Jorge R <jorgerodriguez434@gmail.com>
2018-01-24 12:17:13 -0600

    added semantic form in index.html and removed functions from js file

--- youtube.js
+++ youtube.js
@@ -8,36 +8,6 @@ function getDataFromYouTubeApi(searchTerm, callback) {
   }
   $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
 }
-function renderResult(result) {
-  return `
-    <div>
-      <h2>
-      <a class="js-result-name" href="${result.html_url}" target="_blank">${result.name}</a> by <a class="js-user-name" href="${result.owner.html_url}" target="_blank">${result.owner.login}</a></h2>
-      <p>Number of watchers: <span class="js-watchers-count">${result.watchers_count}</span></p>
-      <p>Number of open issues: <span class="js-issues-count">${result.open_issues}</span></p>
-    </div>
-  `;
-}
-function displayYouTubeSearchData(data) {
-  const results = data.items.map((item, index) => renderResult(item));
-  $(".js-testing-search-button-result").html(results);
-}
-function watchSubmit() {
-  $(".js-youtube-search-form").submit(event => {
-    event.preventDefault();
-    const queryTarget = $(event.currentTarget).find(".js-query");
-    const query = queryTarget.val();
-    // clear out the input
-    queryTarget.val("");
-    getDataFromYouTubeApi(query, displayYouTubeSearchData);
-  });
-}
-
-
-
-
-
-
 function handleSearchButton(){
   $(".js-testing-search-button-result").text("Get thumbnails of the videos.").hide();
   //when the submit button is clicked, show the text.
@@ -59,7 +29,6 @@ function handleResetButton(){
 function handleEveything(){
   handleSearchButton();
   handleResetButton();
-//  watchSubmit();
 }
 
 $(handleEveything)
