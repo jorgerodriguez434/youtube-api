80a3115847329a6f3d702882f90189bf15efb676
Jorge R <jorgerodriguez434@gmail.com>
2018-01-25 16:37:00 -0600

    modified mentorApi.js and made it my own. Have a better understanding

--- mentorApi.js
+++ mentorApi.js
@@ -31,61 +31,24 @@ $('.search').click(fetch); */
 // as an argument and then executed mostly passing in
 // some data
 
-function fetcher5(anyFunctionPassed5) {
-  var fakeArray5 = [{name: 'JO'},{name: 'John'}];
-  anyFunctionPassed5(fakeArray5); 
-}
-function iterator5(array5){
-  //going to iterate through an Array
-  array5.map(person6 =>{
-            console.log("This person is " + person6.name);
+function appendUsers(users){
+  users.map(person => {
+    $(".results").append(`<li>  ${person.name}`);
   });
 }
-
-
-
-
 function getUsers(data) {
   console.log('1');
   appendUsers(data);
 }
 
-function error(err) {
-  console.log(err);
-}
-function appendUsers(users) {
-  for(var i = 0; i < users.length; i += 1) {
-    $('.results').append('<li>'+users[i].name);
-  }
-}
-/*
-function fetch() {
-  $.ajax('https://jsonplaceholder.typicode.com/users')
-  .then(getUsers)
-  .catch(error);
-  console.log('2');
-}
-
-$('.search').click(fetch); */
-
 function handleEverything(){
- /*fetcher5( info5 => {
-        console.log(info);
-});*/
-/*fetcher5(info5 => {
-      info5.map(person5 => {
-            console.log("This person is " + person.name);
-      });
-});*/
-//fetcher5(iterator5);
-
-
-
-
+ 
+  $.ajax("https://jsonplaceholder.typicode.com/users")
+  .then(getUsers);
 
 }//handleEverything
 
-handleEverything();
+$(".search").on("click", handleEverything);
 
 
 
