a8bea753a09d2a35c5a740612b89018bdb13863e
Jorge R <jorgerodriguez434@gmail.com>
2018-01-26 00:47:32 -0600

    full understanding of callback functions--modified

--- mentorApi.js
+++ mentorApi.js
@@ -1,30 +1,27 @@
-const URL = "https://jsonplaceholder.typicode.com/users";
+const PRACTICE_URL = "https://jsonplaceholder.typicode.com/users";
 const PRACTICE_INFO = [{name:"Jorge"}, {name: "Erika"}];
-
+const GITHUB_SEARCH_URL = 'https://api.github.com/search/repositories';
 
 function display(arrayOfData){
-  //this is going to transform the data received
-  //into HTML format
+  //this is going to display the data received
   console.log("\'display function executed\'")
   arrayOfData.map(user => {
-          console.log(user.name +": "+ user.phone);
+          console.log(user.email);
           
-          $('.results').append("<li>" + user.name+": "+user.phone);
+          $('.results').append("<li>" + user.email);
   });
+}
+function handleSearchButton(anyFunction){
+$('.search-button').on('click', anyFunction);
 }
 function main(){
   //this is going to execute the functions
-  $.ajax(URL).then(display);
+  $.ajax(PRACTICE_URL).then(display);
   //display(PRACTICE_INFO);
   
 }
-main();
 
-
-
-
-
-
+handleSearchButton(main);
 
 
 
