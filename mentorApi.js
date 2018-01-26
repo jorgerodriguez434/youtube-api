b086e6dc73132d2e4caad428f9e6847eadfd1fe8
Jorge R <jorgerodriguez434@gmail.com>
2018-01-26 12:49:30 -0600

    modified mentorApi.js-- derived data from https://api.github.com/search/repositories?q=thinkful%20in:name&page=1&per_page=5

--- mentorApi.js
+++ mentorApi.js
@@ -1,31 +1,27 @@
 const PRACTICE_URL = "https://jsonplaceholder.typicode.com/users";
 const PRACTICE_INFO = [{name:"Jorge"}, {name: "Erika"}];
-const GITHUB_SEARCH_URL = 'https://api.github.com/search/repositories';
+const GITHUB_SEARCH_URL = 'https://api.github.com/search/repositories?q=thinkful%20in:name&page=1&per_page=5';
 
-function display(arrayOfData){
+function renderData(arrayOfData){
   //this is going to display the data received
   console.log("\'display function executed\'")
-  arrayOfData.map(user => {
-          console.log(user.email);
-          
-          $('.results').append("<li>" + user.email);
+  arrayOfData.items.map(index => {
+          console.log(index.owner.login);
+          $('.results').append(`<li> ${index.owner.login} `);
   });
 }
-function handleSearchButton(anyFunction){
-$('.search-button').on('click', anyFunction);
+
+function handleSearchButton(anyFunctionPassed){
+$('.search-button').on('click', anyFunctionPassed);
 }
-function main(){
+
+function fetch(){
   //this is going to execute the functions
-  $.ajax(PRACTICE_URL).then(display);
-  //display(PRACTICE_INFO);
-  
+  $.ajax(GITHUB_SEARCH_URL).then(renderData);
+  //renderData(PRACTICE_INFO);
 }
 
-handleSearchButton(main);
-
-
-
-
+handleSearchButton(fetch);
 
 
 
