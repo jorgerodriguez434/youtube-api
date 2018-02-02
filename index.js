93d2f7171eb1c9be2a5988a4f89981d0f1d5ce61
Jorge R <jorgerodriguez434@gmail.com>
2018-02-02 00:21:30 -0600

    made it mostly accesible

--- index.js
+++ index.js
@@ -3,20 +3,22 @@ const YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search";
 function render(data){
   $('.results').empty();
   let count = 0;
+  console.log(data.kind);
   data.items.map(video  => {
+          count++;
           $('.results').append(
           `
           <li> <h1>${video.snippet.title}</h1>
                <p>${video.snippet.description}</p>
                <!-- <img src = "${video.snippet.thumbnails.medium.url}"> -->
+               <a href="https://www.youtube.com/watch?v=${video.id.videoId}"> Watch on YouTube</a>
                <iframe src="https://www.youtube.com/embed/${video.id.videoId}" title="video" tabindex="${count}"</iframe>
  
-          `)
+          `);
   });
 }
 
 function fetch(searchTerm, callBack){
-  console.log(searchTerm);
   const params = {
     part: 'snippet', 
     key: "AIzaSyBrEPE9JGPWkYpX06O2gMOloYSgUKXcMhQ",
@@ -29,7 +31,6 @@ function fetch(searchTerm, callBack){
 $('.search-button').on('click', event => {
   event.preventDefault();
   let searchTerm = $('input').val();
-  console.log(searchTerm);
   fetch(searchTerm, render)
  
 });
